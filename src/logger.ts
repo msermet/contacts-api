5c95567e2c2059a96be7343955e7bbfabe310a2c
Clément Da Silva <clement@MacBook-Air-de-Clement.local>
2025-11-06 13:44:30 +0100

    ajout du stockage de log dans un fichier

--- src/logger.ts
+++ src/logger.ts
@@ -3,9 +3,19 @@ import pino from 'pino';
 export const logger = pino({
     level: 'info',
     transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
-        }
+        targets: [
+            {
+                target: 'pino/file',
+                options: {
+                    destination: './app.log'
+                }
+            },
+            {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true
+                }
+            }
+        ]
     }
 });
